b65900fb0dfefe39effba2ac2bdd987a925361c8
spyrash <diego.petrucci@outlook.com>
2025-06-15 16:02:12 +0200

    feat: used CommandLineRunner

---
+++ src/main/java/CRUD/demo/ProvaCrudApplication.java
@@ -0,0 +1,21 @@
+package CRUD.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProvaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaCrudApplication.class, args);
+	}
+
+	@Bean
+    protected CommandLineRunner unoCiProva(){
+		return args -> {
+			System.out.println("Prova Crud Application is running!");
+		};
+	}
+}
