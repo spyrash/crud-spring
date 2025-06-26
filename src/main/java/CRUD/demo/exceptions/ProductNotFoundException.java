46156c33c97d667e2256565c9246d1c2ec663b29
spyrash <diego.petrucci@outlook.com>
2025-06-26 14:42:45 +0200

    feat: added logger

--- src/main/java/CRUD/demo/exceptions/ProductNotFoundException.java
+++ src/main/java/CRUD/demo/exceptions/ProductNotFoundException.java
@@ -1,11 +1,15 @@
 package CRUD.demo.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException {
+    private static final Logger logger = LoggerFactory.getLogger(ProductNotFoundException.class);
     public ProductNotFoundException() {
         super("Product not found");
+        logger.error("exception {} thrown", getClass());
     }
 }
--- src/main/java/CRUD/demo/product/services/GetProductService.java
+++ src/main/java/CRUD/demo/product/services/GetProductService.java
@@ -5,6 +5,8 @@ import CRUD.demo.product.ProductRepository;
 import CRUD.demo.product.Query;
 import CRUD.demo.product.model.Product;
 import CRUD.demo.product.model.ProductDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ import java.util.Optional;
 @Service
 public class GetProductService implements Query<Integer, ProductDto> {
     private final ProductRepository productRepository;
+    private static final Logger logger = LoggerFactory.getLogger(GetProductService.class);
 
     public GetProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
@@ -20,6 +23,7 @@ public class GetProductService implements Query<Integer, ProductDto> {
 
     @Override
     public ResponseEntity<ProductDto> execute(Integer productId) {
+        logger.info("executing{} with input: {}", getClass(), productId);
         Optional<Product> productOptional = productRepository.findById(productId);
         return productOptional.map(product -> ResponseEntity.ok(new ProductDto(product)))
                 .orElseThrow(ProductNotFoundException::new);
