60329f598e2d7af6f8d3fa7cafd2b905dbe5629f
spyrash <diego.petrucci@outlook.com>
2025-06-26 15:20:59 +0200

    feat: added scheduler to clear cache, tried custom configurations

---
+++ src/main/java/CRUD/demo/CacheConfiguration.java
@@ -0,0 +1,33 @@
+package CRUD.demo;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableCaching
+@EnableScheduling
+public class CacheConfiguration {
+
+    @Bean  // the return value of th method get injected into the spring container using dependency injection
+    public CacheManager cacheManager() {
+        ConcurrentMapCacheManager manager = new ConcurrentMapCacheManager();
+        manager.setAllowNullValues(false);
+        manager.setCacheNames(Arrays.asList("productCache"));
+        return manager;
+    }
+
+    @CacheEvict(value = "productCache", allEntries = true)
+    @Scheduled(fixedRate = 7000, initialDelay = 0) // every 7 seconds
+    public void evictProductCache() {
+        System.out.println("evicting product cache...");
+        System.out.println("done!");
+    }
+}
